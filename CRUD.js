9905229e6781d21aa1d1115f7b59bdd14d319a58
Nguyễn Minh Tuấn <tuannm1610@gmail.com>
2022-05-11 08:48:03 +0700

    function edit ok

--- CRUD.js
+++ CRUD.js
@@ -64,7 +64,7 @@ function getBook(book) {
                        <td>${book.name}</td>
                         <td>${book.price}</td>
                         <td>${book.category.name}</td>
-                        <td><a href="" onclick="">Edit</a>&nbsp;<a href="${book.id}" onclick="deleteBook(this)">Delete</a></td></tr>`;
+                        <td><a href="${book.id}" onclick="showBookForm(${book.id})">Edit</a>&nbsp;<a href="${book.id}" onclick="deleteBook(this)">Delete</a></td></tr>`;
     return bookChoosen;
 }
 
@@ -83,3 +83,82 @@ function deleteBook(book) {
         })
     }
 }
+
+function showBookForm(id) {
+    event.preventDefault();
+    $.ajax({
+        type: "GET",
+        url: `http://localhost:8080/books/${id}`,
+        success: function (data) {
+            console.log(data);
+            let content = `<h2>Update a book</h2>
+    <form>
+        <table>
+            <tr>
+                <th><label for="author1">Author:</label></th>
+                <td><input id="author1" name="author1" type="text" value="${data.author}"></td>
+            </tr>
+            <tr>
+                <th><label for="name1">Name:</label></th>
+                <td><input id="name1" name="name1" type="text" value="${data.name}"></td>
+            </tr>
+            <tr>
+                <th><label for="price1">Price:</label></th>
+                <td><input id="price1" name="price1" type="number" value="${data.price}"></td>
+            </tr>
+            <tr>
+                <th><label for="category1">Category:</label></th>
+                <td><select id="category1" name="category1"></select></td>
+            </tr>
+            <tr>
+                <th>&nbsp;</th>
+                <td><input onclick="editBook(${data.id})" type="button" value="Update book"></td>
+            </tr>
+        </table>
+    </form>`;
+            document.getElementById("editForm").innerHTML = content;
+            displayCategory1();
+        }
+    });
+}
+
+function displayCategory1() {
+    let content = "";
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/books/category",
+        success: function (data) {
+            for (let i = 0; i < data.length; i++) {
+                content += `<option value="${data[i].id}">${data[i].name}</option>`
+            }
+            document.getElementById("category1").innerHTML = content;
+        }
+    })
+}
+
+function editBook(id) {
+    let author = $(`#author1`).val();
+    let name = $(`#name1`).val();
+    let price = $(`#price1`).val();
+    let category = $(`#category1`).val();
+    let bookEdit = {
+        "author": author,
+        "name": name,
+        "price": price,
+        "category": {"id": category}
+    };
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        type: "PUT",
+        data: JSON.stringify(bookEdit),
+        url: `http://localhost:8080/books/${id}`,
+        success: function () {
+            alert("Book updated successfully");
+            listBooks();
+        }
+    });
+}
+
